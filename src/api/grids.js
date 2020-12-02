6c4a7325060af3933b3cfd208c69440a1f371d68
Maggie <13256724140@163.com>
2020-12-02 16:56:48 +0800

    重新init

--- src/api/grids.js
+++ src/api/grids.js
@@ -1,26 +1,26 @@
 /*
  * @Author: your name
  * @Date: 2020-10-26 14:56:23
- * @LastEditTime: 2020-11-19 16:06:24
+ * @LastEditTime: 2020-12-02 16:50:22
  * @LastEditors: 赵婷婷
  * @Description: In User Settings Edit
  * @FilePath: \manuscript-pc\src\api\common.js
  */
-import gridsAxios from "ym-bridge-shandianyun";
+// import gridsAxios from "ym-bridge-shandianyun";
 
-let basePath = "https://shandianyun-extraction.iqilu.com";
+let basePath = 'https://shandianyun-extraction.iqilu.com';
 
-export const getGridsList = params => {
-  return gridsAxios.request({
-    url: basePath + "/xxx",
-    method: "get",
-    params
-  });
-};
+// export const getGridsList = params => {
+//   return gridsAxios.request({
+//     url: basePath + "/xxx",
+//     method: "get",
+//     params
+//   });
+// };
 
-export const getSyncGridUrl = id => {
-  return gridsAxios.request({
-    url: basePath + "/sync?id=" + id,
-    method: "get"
-  });
-};
+// export const getSyncGridUrl = id => {
+//   return gridsAxios.request({
+//     url: basePath + "/sync?id=" + id,
+//     method: "get"
+//   });
+// };
