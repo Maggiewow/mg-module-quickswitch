ba0b90a8fc8ac757a2bd1410c8ca90d4976277f9
Maggiewow <13256724140@163.com>
2022-12-26 14:55:47 +0800

    requestUrl

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-07-23 09:48:43
- * @LastEditTime: 2021-05-10 14:37:29
+ * @LastEditTime: 2022-12-26 14:49:58
  * @LastEditors: 赵婷婷
  * @Description: In User Settings Edit
  * @FilePath: \sucai-modal\src\views\Home.vue
@@ -9,17 +9,27 @@
 <template>
   <div class="comps">
     <div class="home comp-grids">
-      <grids-jump :collapsed="collapsed" theme="dark" @on-back="backLogin" evn="dev"></grids-jump>
+      <grids-jump
+        :collapsed="collapsed"
+        theme="dark"
+        @on-back="backLogin"
+        :requestUrl="baseRegion"
+      ></grids-jump>
     </div>
     <div class="home comp-grids comp-grids-light">
-      <grids-jump :collapsed="collapsed" theme="light" @on-back="backLogin" evn="dev"></grids-jump>
+      <grids-jump
+        :collapsed="collapsed"
+        theme="light"
+        @on-back="backLogin"
+        :requestUrl="baseRegion"
+      ></grids-jump>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import GridsJump from '@/components/grids-jump';
+import GridsJump from '@/components/grids-jump'
 
 export default {
   name: 'Home',
@@ -27,15 +37,15 @@ export default {
     GridsJump,
   },
   data() {
-    return { collapsed: false };
+    return { collapsed: false, baseRegion: 'https://user.shandian8.com' }
   },
   mounted() {},
   methods: {
     backLogin() {
-      console.log('home页面--返回首页');
+      console.log('home页面--返回首页')
     },
   },
-};
+}
 </script>
 
 <style scoped>
